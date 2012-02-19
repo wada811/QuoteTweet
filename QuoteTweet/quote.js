6a2c3f7e6d5e4de4b11e7672995bf817fd2bc0e0
wada811 <89.at.usi@gmail.com>
2012-02-19 23:51:05 +0900

    first commit

---
+++ QuoteTweet/quote.js
@@ -0,0 +1,36 @@
+﻿function QuoteTweet(){
+	var statusBoxID = ".twitter-anywhere-tweet-box-editor:last";
+	var QTTag = '<li class="action-old-retweet-container quote-tweet-link"><a class="with-icn" href="#" title="Quote Tweet"> <i class="action-rt"></i><b>QT</b></a> </li>';
+
+	var username = jQuery.trim($("#screen-name:first").text());
+
+	$(".js-stream-item").live('mouseover',function(){
+		if ($(this).find(".quote-tweet-link").length == 0 ){
+			$(this).find(".client-and-actions .js-actions").append(QTTag).find(".quote-tweet-link").click(function(){
+				var tweet = $(this).parent().parent().parent().parent().parent();
+				var tweettext = " QT https://twitter.com" +  tweet.find(".js-permalink").attr("href");
+				var expandedUrl = "";
+				tweet.find(".twitter-timeline-link").each(function(){
+					expandedUrl += " " + $(this).attr("data-expanded-url");
+				});
+				tweettext += expandedUrl;
+				new twttr.widget.TweetDialog({
+					template:{title:"Quote Tweet"},
+					modal: true,
+					draggable: true,
+					defaultContent: tweettext,
+					origin: "new-tweet-titlebar-button"
+				}).open().focus();
+				return false;
+			});
+		}
+	});
+}
+
+$(document).ready(function() {
+	var node = document.createElement('script');
+	var toInject = "("+QuoteTweet.toString()+")();";
+
+	node.innerText = toInject ;
+	document.querySelector('body').appendChild(node);
+});
